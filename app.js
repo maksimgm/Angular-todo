e0e0106682a1ad63e61e24bf38d1183198f8e352
maksimgm <maksimmamrikov@gmail.com>
2015-11-13 11:53:23 -0800

    FINISHED IT!!!

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-app = angular.module('todoApp'); // What's missing? Hint, it's more than one thing.
+var app = angular.module('todoApp', []); // What's missing? Hint, it's more than one thing.
 
-app.controller("TodosController", function() { // What's missing?
-	$scope.todos = ; // Store todo items here. What is the data type?
-	function addTodo() { // Rewrite to make this function abailable in your view
+app.controller("TodosController", function($scope) { // What's missing?
+	$scope.todos = {}; // Store todo items here. What is the data type?
+  $scope.todos.data = [];
+  $scope.todos.newTodo = {};
+	
+  $scope.addTodo = function(newTodo) { // Rewrite to make this function abailable in your view
 		// Add code here
-	};
-	function deleteAll() { // Rewrite to make this function abailable in your view
-		// Add code here
-	};
+    $scope.todos.data.push(newTodo);
+		$scope.todos.newTodo = {};
+  };
+  
+  $scope.deleteAll = function(newTodo) { // Rewrite to make this function abailable in your view
+	  $scope.todos.data = [];
+  };
 });
